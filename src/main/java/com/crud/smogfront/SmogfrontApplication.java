84a8abbb45c69feff4b0114e67195272c3387c98
lukaszlizewski <lizewski.lukasz@gmail.com>
2021-02-19 20:32:25 +0100

    first commit

---
+++ src/main/java/com/crud/smogfront/SmogfrontApplication.java
@@ -0,0 +1,13 @@
+package com.crud.smogfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmogfrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmogfrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crud/smogfront/SmogfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.smogfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmogfrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
